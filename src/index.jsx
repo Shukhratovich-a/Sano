3009281ba1dbba46d6af68357dbb35e3511b619f
shukhratovich-a <shukhratovich138@gmail.com>
2022-09-17 18:35:12 +0500

    Connect section provider

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { Provider as MenuProvider } from "./Context/Menu";
+import { Provider as SectionProvider } from "./Context/Seciton";
 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MenuProvider>
-      <App />
-    </MenuProvider>
+    <SectionProvider>
+      <MenuProvider>
+        <App />
+      </MenuProvider>
+    </SectionProvider>
   </React.StrictMode>
 );
